c10980e0bd475dd80fea047e56bd11ab12526a85
Aidan Malone <g00423306@atu.ie>
2025-01-08 16:35:27 +0000

    added more dataloaders

--- src/main/java/ie/atu/week11example/DataLoader.java
+++ src/main/java/ie/atu/week11example/DataLoader.java
@@ -21,12 +21,22 @@ public class DataLoader implements CommandLineRunner {
         Mountain testData2 = new Mountain(2L, "Toubkal", "Aztat treks", 500, "aztattreks@gmail.com", "5 days", "Morocco", "Atlas Mountains");
         Mountain testData3 = new Mountain(3L, "Mera", "MuchBetterAdventures", 3800, "muchbetteradventures@gmail.com", "14 days", "Nepal", "Himalayas");
         Mountain testData4 = new Mountain(4L, "Mera", "Trek Central", 4200, "mail@trekcentralpal.com", "19 days", "Nepal", "Himalayas");
-        Mountain testData5 = new Mountain(5L, "Mont Blanc", "Explore Share", 3400, "info@exploreshare.com", "5 days", "France", "French Alps");
+        Mountain testData5 = new Mountain(5L, "Mont Blanc", "Explore Share", 3400, "info@exploreshare.com", "5 days", "France", "Alps");
+        Mountain testData6 = new Mountain(6L, "Ben Nevis", "Maximum Adventure", 100, "mail@maximumadventure.com", "1 days", "Scotland", "Western HighLands");
+        Mountain testData7 = new Mountain(7L, "Kilimanjaro", "TourCompass", 2800, "info@tourcampass.co.uk", "10 days", "Tanzania", "Rift Mountains");
+        Mountain testData8 = new Mountain(8L, "Snowden", "Maximum Adventure", 100, "mail@maximumadventure.com", "1 days", "Wales", "Snowdonia");
+        Mountain testData9 = new Mountain(9L, "Matterhorn", "Alpine Ascents", 7250, "Climb@AlpineAscents.com", "3 days", "Switzerland", "French Alps");
+        Mountain testData10 = new Mountain(10L, "Mont Blanc", "MuchBetterAdventures", 3285, "muchbetteradventures@gmail.com", "8 days", "France", "Alps");
         mountainRepository.save(testData);
         mountainRepository.save(testData2);
         mountainRepository.save(testData3);
         mountainRepository.save(testData4);
         mountainRepository.save(testData5);
+        mountainRepository.save(testData6);
+        mountainRepository.save(testData7);
+        mountainRepository.save(testData8);
+        mountainRepository.save(testData9);
+        mountainRepository.save(testData10);
     }
 
 }
